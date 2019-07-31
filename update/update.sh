a84ec592da1fa0aaab1576eea9e4422e4b95e332
Joe Sustaric <joe.sustaric@envato.com>
2019-07-31 15:21:00 +1000

    better comment

--- update/update.sh
+++ update/update.sh
@@ -2,8 +2,8 @@
 set -euo pipefail
 
 # Bundle updating may download gems from git
-# need to add github.com host key to known_hosts
-# or the build will hang at accept prompt
+# we need to add github.com host key to known_hosts file
+# or the build will hang at the prompt to accept the key
 ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts
 
 cd /bundle_update
