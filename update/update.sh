4548deaceaacf269f72b00e299374353957cc5a7
Joe Sustaric <joe.sustaric@envato.com>
2019-07-31 15:18:21 +1000

    typo

--- update/update.sh
+++ update/update.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-# Bundle updating may download gems for git
+# Bundle updating may download gems from git
 # need to add github.com host key to known_hosts
 # or the build will hang at accept prompt
 ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts
