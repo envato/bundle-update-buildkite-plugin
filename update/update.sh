01ebec416cf17e1a44783bcfebaa7a88e1b9698e
Joe Sustaric <joe.sustaric@envato.com>
2019-07-31 15:11:03 +1000

    add github host key to the known_hosts before bundle updating

--- update/update.sh
+++ update/update.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+# Bundle updating may download gems for git
+# need to add github.com host key to known_hosts
+# or the build will hang at accept prompt
+ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts
+
 cd /bundle_update
 bundle update --jobs="$(nproc)"
