a1418220a659ebf2a4d40949c700563fc5a027b3
Joe Sustaric <joe.sustaric@envato.com>
2019-08-01 11:39:33 +1000

    remove debugging and create the known_hosts file

--- update/update.sh
+++ update/update.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
-set -euox pipefail
+set -euo pipefail
 
 # Bundle updating may download gems from git
 # we need to add github.com host key to known_hosts file
 # or the build will hang at the prompt to accept the key
+mkdir -p ~/.ssh && touch ~/.ssh/known_hosts
 ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts
 
 cd /bundle_update
