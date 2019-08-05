d879eedf1adab5be54ca74e20056ced2a37cd24f
Joe Sustaric <joe.sustaric@envato.com>
2019-08-05 15:16:31 +1000

    back to simple impl

--- update/update.sh
+++ update/update.sh
@@ -3,20 +3,6 @@ set -euo pipefail
 
 cd /bundle_update
 
-echo "pre_bundle_update=${PRE_BUNDLE_UPDATE}"
-
-echo "BUILDKITE_PLUGIN_BUNDLE_UPDATE_PRE_BUNDLE_UPDATE=${BUILDKITE_PLUGIN_BUNDLE_UPDATE_PRE_BUNDLE_UPDATE}"
-pre_bundle_update=${PRE_BUNDLE_UPDATE}
-if [ -f "$pre_bundle_update" ]; then
-    echo "$pre_bundle_update exist"
-else
-    echo "$pre_bundle_update does not exist"
-    pre_bundle_update="./buildkite/scripts/pre-bundle-update"
-fi
-
-eval ".buildkite/scripts/pre-bundle-update"
-# custom defined script
-# run it..
-
+. "buildkite/scripts/pre-bundle-update"
 
 bundle update --jobs="$(nproc)"
