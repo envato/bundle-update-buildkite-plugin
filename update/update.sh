6e617efbc4a89c893a215ccccf52d808e56fc556
Joe Sustaric <joe.sustaric@envato.com>
2019-08-05 10:16:15 +1000

    sourcing the fucntion

--- update/update.sh
+++ update/update.sh
@@ -3,6 +3,11 @@ set -euo pipefail
 
 cd /bundle_update
 
+PLUGIN_DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)/.."
+
+# shellcheck source=lib/config.sh
+. "$PLUGIN_DIR/lib/config.sh"
+
 pre_bundle_update=$(plugin_read_config PRE_BUNDLE_UPDATE)
 
 if [ -f "$pre_bundle_update" ]; then
