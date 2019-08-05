748ca3494b205ae545e9f1c547e483d37a7c3359
Joe Sustaric <joe.sustaric@envato.com>
2019-08-05 14:46:35 +1000

    testing

--- update/update.sh
+++ update/update.sh
@@ -1,17 +1,9 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-
-function plugin_read_config() {
-  local var="BUILDKITE_PLUGIN_BUNDLE_UPDATE_${1}"
-  local default="${2:-}"
-  echo "${!var:-$default}"
-}
-
-
 cd /bundle_update
-pre_bundle_update=$(plugin_read_config PRE_BUNDLE_UPDATE)
-echo "pre_bundle_update=${pre_bundle_update}"
+
+echo "pre_bundle_update=${PRE_BUNDLE_UPDATE}"
 
 if [ -f "$pre_bundle_update" ]; then
     echo "$pre_bundle_update exist"
