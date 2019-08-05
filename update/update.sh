a3230f8b5d9d39871d0fedc7483a3b7b1577bbae
Joe Sustaric <joe.sustaric@envato.com>
2019-08-05 15:31:33 +1000

    check if file exists

--- update/update.sh
+++ update/update.sh
@@ -3,6 +3,9 @@ set -euo pipefail
 
 cd /bundle_update
 
-eval ".buildkite/scripts/pre-bundle-update"
+if [ -f ".buildkite/scripts/pre-bundle-update" ]; then
+    echo "Installing custom dependencies..."
+    eval ".buildkite/scripts/pre-bundle-update"
+fi
 
 bundle update --jobs="$(nproc)"
