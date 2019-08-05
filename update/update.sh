262b7a273f479a53f8c3ae0cfd0e877c7844e6b1
Joe Sustaric <joe.sustaric@envato.com>
2019-08-05 10:10:23 +1000

    parameter for the script file

--- update/update.sh
+++ update/update.sh
@@ -3,8 +3,16 @@ set -euo pipefail
 
 cd /bundle_update
 
-# check if hook file exists.
-.buildkite/scripts/pre-bundle-update
+pre_bundle_update=$(plugin_read_config PRE_BUNDLE_UPDATE)
+
+if [ -f "$pre_bundle_update" ]; then
+    echo "$pre_bundle_update exist"
+else
+    echo "$pre_bundle_update does not exist"
+    pre_bundle_update="./buildkite/scripts/pre-bundle-update"
+fi
+
+eval pre_bundle_update
 # custom defined script
 # run it..
 
