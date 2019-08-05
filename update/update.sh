ded82220537797bf0ec8af6f8cd8595b454f27bf
Joe Sustaric <joe.sustaric@envato.com>
2019-08-05 10:18:58 +1000

    dont cd just yet

--- update/update.sh
+++ update/update.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-cd /bundle_update
-
 PLUGIN_DIR="$(cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd)/.."
 
 # shellcheck source=lib/config.sh
 . "$PLUGIN_DIR/lib/config.sh"
 
+cd /bundle_update
 pre_bundle_update=$(plugin_read_config PRE_BUNDLE_UPDATE)
+echo "pre_bundle_update=${pre_bundle_update}"
 
 if [ -f "$pre_bundle_update" ]; then
     echo "$pre_bundle_update exist"
@@ -20,5 +20,6 @@ fi
 eval pre_bundle_update
 # custom defined script
 # run it..
+
 
 bundle update --jobs="$(nproc)"
