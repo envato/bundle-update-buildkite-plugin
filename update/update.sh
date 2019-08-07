56c180cfe71a72f15b25f0836fa8f2377c655739
Joe Sustaric <joe.sustaric@envato.com>
2019-08-07 14:25:38 +1000

    shell check

--- update/update.sh
+++ update/update.sh
@@ -3,9 +3,9 @@ set -euo pipefail
 
 cd /bundle_update
 
-if [ -f $PRE_BUNDLE_UPDATE ]; then
+if [ -f "$PRE_BUNDLE_UPDATE" ]; then
   echo "Running pre install script..."
-  eval $PRE_BUNDLE_UPDATE
+  eval "$PRE_BUNDLE_UPDATE"
 fi
 
 bundle update --jobs="$(nproc)"
