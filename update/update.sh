77a98cfe01cc5ac3c7849c979ac6290947a5c1a0
Joe Sustaric <joe.sustaric@envato.com>
2019-08-09 10:59:18 +1000

    run whatever is passed in script or command, it should exit and fail if the command or script return a non zero exit code

--- update/update.sh
+++ update/update.sh
@@ -3,12 +3,7 @@ set -euo pipefail
 
 cd /bundle_update
 
-if [ -f "$PRE_BUNDLE_UPDATE" ]; then
-  echo "Running pre install script..."
-  eval "$PRE_BUNDLE_UPDATE"
-else
-  echo "Specified file location does not exist $PRE_BUNDLE_UPDATE"
-  exit 1
-fi
+echo "Running pre install script..."
+eval "$PRE_BUNDLE_UPDATE"
 
 bundle update --jobs="$(nproc)"
