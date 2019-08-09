43fdc1d7897f87352df841531cb0ada4b8fff17c
Joe Sustaric <joe.sustaric@envato.com>
2019-08-09 10:56:33 +1000

    fail when a specified file does not exist

--- update/update.sh
+++ update/update.sh
@@ -8,6 +8,7 @@ if [ -f "$PRE_BUNDLE_UPDATE" ]; then
   eval "$PRE_BUNDLE_UPDATE"
 else
   echo "Specified file location does not exist $PRE_BUNDLE_UPDATE"
+  exit 1
 fi
 
 bundle update --jobs="$(nproc)"
