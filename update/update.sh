7efd404238790f195763e98debb485fc55ecdf78
Orien Madgwick <_@orien.io>
2019-09-02 10:58:41 +1000

    Only log and eval if a command has been provided

--- update/update.sh
+++ update/update.sh
@@ -3,11 +3,15 @@ set -euo pipefail
 
 cd /bundle_update
 
-echo "--- :shell: Running pre bundle update"
-eval "$PRE_BUNDLE_UPDATE"
+if [ -n "$PRE_BUNDLE_UPDATE" ]; then
+  echo "--- :shell: Running pre bundle update"
+  eval "$PRE_BUNDLE_UPDATE"
+fi
 
 echo "+++ :bundler: Running bundle update"
 bundle update --jobs="$(nproc)"
 
-echo "--- :shell: Running post bundle update"
-eval "$POST_BUNDLE_UPDATE"
+if [ -n "$POST_BUNDLE_UPDATE" ]; then
+  echo "--- :shell: Running post bundle update"
+  eval "$POST_BUNDLE_UPDATE"
+fi
