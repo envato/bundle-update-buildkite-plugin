0e31799423e435b76117b64485dc21b4cc165b56
Peter Vandoros <peter.vandoros@envato.com>
2021-05-12 00:59:17 +1000

    Specify the --lock-file arg to unwrappr when annotating a PR

--- unwrappr/annotate.sh
+++ unwrappr/annotate.sh
@@ -11,6 +11,17 @@ bundle install --jobs="$(nproc)"
 echo "+++ :github: Annotating Github pull request"
 repository=$1
 pull_request=$2
+gemfile_lock_files=("${@:3}")
+
+if [[ ${#gemfile_lock_files[@]} -eq 0 ]]; then
+  gemfile_lock_files+=("--lock-file" "Gemfile.lock")
+fi
+
 echo "Annotating https://github.com/${repository}/pull/${pull_request}"
+echo "Files: " "${gemfile_lock_files[@]}"
 echo
-bundle exec unwrappr annotate-pull-request --repo "${repository}" --pr "${pull_request}"
+
+bundle exec unwrappr annotate-pull-request \
+  --repo "${repository}" \
+  --pr "${pull_request}" \
+  "${gemfile_lock_files[@]}"
